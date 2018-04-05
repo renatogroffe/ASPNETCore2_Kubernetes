33f950588db1fd8223f7fc9ff02d3892a77fe3bc
Renato Groffe <renato.groffe@yahoo.com.br>
2018-04-05 01:15:47 -0300

    ASP.NET Core 2.0 + Kubernetes + Azure
    Exemplo utilizando ASP.NET Core 2.0, Kubernetes e Microsoft Azure

---
+++ APIContagem/APIContagem/Contador.cs
@@ -0,0 +1,14 @@
+﻿namespace APIContagem
+{
+    public class Contador
+    {
+        private int _valorAtual = 0;
+
+        public int ValorAtual { get => _valorAtual; }
+
+        public void Incrementar()
+        {
+            _valorAtual++;
+        }
+    }
+}
---
+++ APIContagem/APIContagem/Controllers/ContadorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIContagem.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContadorController : Controller
+    {
+        private static Contador _CONTADOR = new Contador();
+
+        [HttpGet]
+        public object Get()
+        {
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+
+                return new
+                {
+                    _CONTADOR.ValorAtual,
+                    Environment.MachineName,
+                    Sistema = Environment.OSVersion.VersionString
+                };
+            }
+        }
+    }
+}
